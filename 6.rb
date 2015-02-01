fb53936b3e60a7e3bb6054476e4e194ed92bca87
Anne Johnson <annekjohnson23@yahoo.com>
2015-02-01 00:12:03 -0500

    Further optimize 6 and 9

--- 6.rb
+++ 6.rb
@@ -1,12 +1,15 @@
 # PROBLEM 6
 
 def sum_of_squares(n)
-  n == 1 ? 1 : n * n + sum_of_squares(n - 1)
+  (1..n).reduce { |sum, x| sum + x*x }
 end
 
 def square_of_sum(n)
-  (x = (1..n).inject(0) { |result, num| result += num }) * x
+  (1..n).reduce(:+)**2
 end
 
-# answer:
-puts (square_of_sum(100) - sum_of_squares(100))
+# answer with functions:
+# puts (square_of_sum(100) - sum_of_squares(100))
+
+# answer in a one-liner:
+puts (1..100).reduce(:+)**2 - (1..100).reduce { |sum, x| sum + x*x }
--- 9.rb
+++ 9.rb
@@ -1,7 +1,7 @@
 # PROBLEM 9
 
 def get_squares_up_to(n)
-  (1..(Math.sqrt(n).to_i)).map { |num| num * num }
+  (1..(Math.sqrt(n).to_i)).map { |num| num**2 }
 end
 
 def get_pythagorean_triples(squares)
@@ -12,4 +12,4 @@ def where_sum_is(nums, sum)
   nums.detect { |nums| nums.map { |n| Math.sqrt(n).to_i }.reduce(:+) == sum }
 end
 
-puts where_sum_is(get_pythagorean_triples(get_squares_up_to(200000)), 1000).map { |n| Math.sqrt(n).to_i }.reduce(1, :*)
+puts where_sum_is(get_pythagorean_triples(get_squares_up_to(200000)), 1000).map { |n| Math.sqrt(n).to_i }.reduce(:*)
