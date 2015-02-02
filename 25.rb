316e26ebb58f654b7e9c624b68136185c9b9a2c6
Anne Johnson <annekjohnson23@yahoo.com>
2015-02-02 08:59:33 -0500

    Solution to 25

---
+++ 25.rb
@@ -0,0 +1,9 @@
+# PROBLEM 25
+
+cache = [1, 1, 1]
+
+def nth_fib(n, cache)
+  cache[n] || cache[n] = nth_fib(n - 1, cache) + nth_fib(n - 2, cache)
+end
+
+puts (1..Float::INFINITY).find { |n| nth_fib(n, cache).to_s.size === 1000 }
