9b65ab382f22b9ace66a0ae2e65f4363fe4cce7a
Anne Johnson <annekjohnson23@yahoo.com>
2015-02-02 22:42:33 -0500

    Rename local variable

--- 14.rb
+++ 14.rb
@@ -3,10 +3,10 @@
 cache = { "1" => 1 }
 
 def get_collatz_length(n, cache)
-  h_key = n.to_s
-  return cache[h_key] if cache[h_key]
+  key = n.to_s
+  return cache[key] if cache[key]
   nxt = (n.even? ? (n / 2) : (3 * n + 1))
-  cache[h_key] = 1 + get_collatz_length(nxt, cache)
+  cache[key] = 1 + get_collatz_length(nxt, cache)
 end
 
 puts (1...1000000).max_by { |n| get_collatz_length(n, cache) }
